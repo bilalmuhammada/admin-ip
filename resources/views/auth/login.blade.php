00595b4d8e7ee7f971ca4c4907b6441ff57c7bc2
bilalmuhammada <bilalahmada774@gmail.com>
2024-08-27 18:26:48 +0200

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -91,7 +91,7 @@
                                 </div> --}}
                                 <div class="row">
                                     <div class="col-6 text-start">
-                                        <a class="forgot-link" href="{{ env('BASE_URL') }}forgot-password" style="color:#0504aa;">Forgot
+                                        <a class="forgot-link" href="{{ env('BASE_URL') }}/forgot-password" style="color:#0504aa;">Forgot
                                             Password?</a>
                                     </div>
                                     {{-- <div class="col-6 text-end dont-have">New to InfluencerPro? <a
