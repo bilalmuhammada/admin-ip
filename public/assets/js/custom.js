b80b8e2c727e8217f1bf29aa00e804fe12edd47c
bilalmuhammada <bilalahmada774@gmail.com>
2024-09-18 21:55:23 +0200

    Update custom.js

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -157,7 +157,7 @@ function getCitiesByCountry(country_id) {
                 var states = response.data;
                 $("#city_id").empty();
                
-                $("#city_id").append('<option hidden selected disable value=""> &nbsp; &nbsp; </option>');
+                $("#city_id").append('<option hidden selected disable value="">City</option>');
 
                 if (states) {
                     $.each(states, function (index, value) {
