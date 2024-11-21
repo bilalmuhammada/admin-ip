c8811927aed7d45442327ab5a9a5ef567f4bf8da
bilalmuhammada <bilalahmada774@gmail.com>
2024-11-21 11:33:43 +0100

    push

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -93,6 +93,13 @@ font-weight:bold !important;
     padding: 3px 0px 4px 13px !important;
 
 }
+
+.select2-container--default .select2-results__option--highlighted[aria-selected] {
+    background-color: #fff !important;
+    color: blue !important;
+}
+
+
 .select2-search__field:hover{
 border:1px solid blue !important;
 }
@@ -138,27 +145,7 @@ label{
         </div>
         <div class="d-flex justify-content-between align-items-center flex-wrap grid-margin">
             <div class="d-flex align-items-center flex-wrap text-nowrap">
-                <!-- <span>TO: </span>
-                <div class="input-group date datepicker wd-200 me-2 mb-2 mb-md-0" id="dashboardDate">
-                    <span class="input-group-text input-group-addon bg-transparent border-primary"><i
-                            data-feather="calendar" class=" text-primary"></i></span>
-                    <input type="text" class="form-control border-primary bg-transparent">
-                </div>
-                <span>FROM: </span>
-
-                <div class="input-group date datepicker wd-200 me-2 mb-2 mb-md-0" id="EndDate">
-                    <span class="input-group-text input-group-addon bg-transparent border-primary"><i
-                            data-feather="calendar" class=" text-primary"></i></span>
-                    <input type="text" class="form-control border-primary bg-transparent">
-                </div>
-                <button type="button" class="btn btn-outline-primary btn-icon-text me-2 mb-2 mb-md-0">
-                    <i class="btn-icon-prepend" data-feather="printer"></i>
-                    Print
-                </button>
-                <button type="button" class="btn btn-primary btn-icon-text mb-2 mb-md-0">
-                    <i class="btn-icon-prepend" data-feather="download-cloud"></i>
-                    Download Report
-                </button> -->
+               
             </div>
         </div>
 
@@ -631,126 +618,13 @@ label{
                  
                     <!----------------------->
                   
-                <!-- <div class="col-md-3 grid-margin stretch-card">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="d-flex justify-content-between align-items-baseline">
-                                    <h6 class="card-title mb-0">Rated Vendors</h6>
-                                    <div class="dropdown mb-2">
-                                        <button class="btn p-0" type="button" id="dropdownMenuButton"
-                                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <i class="icon-lg text-muted pb-3px" data-feather="more-horizontal"></i>
-                                        </button>
-                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                            <a class="dropdown-item d-flex align-items-center" href="{{ env('BASE_URL') }}vendors?status=RATED"><i
-                                                    data-feather="eye" class="icon-sm me-2"></i> <span
-                                                    class="">View</span></a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-6 col-md-12 col-xl-5">
-                                        <h3 class="mb-2 rated-vendor">0</h3>
-                                        <div class="d-flex align-items-baseline">
-                                            <p class="text-success">
-                                                <span class="rated-vendor-per">+3.3%</span>
-                                              //// {{-- <i data-feather="arrow-up" class="icon-sm mb-1"></i> --}}
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div class="col-6 col-md-12 col-xl-7">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div> -->
-                    <!----------------------->
-                <!-- <div class="col-md-3 grid-margin stretch-card">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="d-flex justify-content-between align-items-baseline">
-                                    <h6 class="card-title mb-0">Favorite Brands</h6>
-                                    <div class="dropdown mb-2">
-                                        <button class="btn p-0" type="button" id="dropdownMenuButton"
-                                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <i class="icon-lg text-muted pb-3px" data-feather="more-horizontal"></i>
-                                        </button>
-                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                            <a class="dropdown-item d-flex align-items-center"
-                                               href="{{ env('BASE_URL') }}vendors?status=FAVORITE"><i
-                                                    data-feather="eye" class="icon-sm me-2"></i> <span
-                                                    class="">View</span></a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-6 col-md-12 col-xl-5">
-                                        <h3 class="mb-2 favorite-vendor">0</h3>
-                                        <div class="d-flex align-items-baseline">
-                                            <p class="text-success">
-                                                <span class="favorite-vendor-per">+3.3%</span>
-                                              //// {{-- <i data-feather="arrow-up" class="icon-sm mb-1"></i> --}}
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div class="col-6 col-md-12 col-xl-7">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div> -->
-                    <!----------------------->
-
-                <!-- <div class="col-md-3 grid-margin stretch-card">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="d-flex justify-content-between align-items-baseline">
-                                    <h6 class="card-title mb-0">Total Vendors</h6>
-                                    <div class="dropdown mb-2">
-                                        <button class="btn p-0" type="button" id="dropdownMenuButton1"
-                                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <i class="icon-lg text-muted pb-3px" data-feather="more-horizontal"></i>
-                                        </button>
-                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                            <a class="dropdown-item d-flex align-items-center" href="{{ env('BASE_URL') }}vendors"><i
-                                                    data-feather="eye" class="icon-sm me-2"></i> <span
-                                                    class="">View</span></a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-6 col-md-12 col-xl-5">
-                                        <h3 class="mb-2 total-vendor">0</h3>
-                                        <div class="d-flex align-items-baseline">
-                                            <p class="text-danger">
-                                                <span>-2.8%</span>
-                                                <i data-feather="arrow-down" class="icon-sm mb-1"></i>
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div class="col-6 col-md-12 col-xl-7">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div> -->
+                
                     <div class="col-md-3 grid-margin stretch-card">
                         <div class="card">
                             <div class="card-body">
                                 <div class="d-flex justify-content-between align-items-baseline">
                                     <h6 class="card-title mb-0">Subscriptions</h6>
-                                    {{-- <div class="dropdown mb-2">
-                                        <button class="btn p-0" type="button" id="dropdownMenuButton2"
-                                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <i class="icon-lg text-muted pb-3px" data-feather="more-horizontal"></i>
-                                        </button>
-                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
-                                            <a class="dropdown-item d-flex align-items-center"
-                                               href="{{ env('BASE_URL') }}influencers/transactions"><i
-                                                    data-feather="eye" class="icon-sm me-2"></i> <span
-                                                    class="">View</span></a>
-                                        </div>
-                                    </div> --}}
+                                    
                                 </div>
                                 <div class="row">
                                     <div class="col-6 col-md-12 col-xl-12">
@@ -846,10 +720,10 @@ label{
             </div>
             <div class="col">
                 
-                <select class="js-example-basic-single form-control form-select city_id" data-width="100%"
+                <select class="js-example-basic-single form-control country_dropdown  form-select city_id" data-width="100%"
                                         id="city_id"
                                         name="city_id">
-                                    <option value=""  > City</option>
+                                    <option value=""  > All Cities</option>
 
                                 </select>
             </div>
@@ -868,12 +742,12 @@ label{
                 </select>
             </div>
             <div class="col">
-                <input type="text" class="form-control datepicker1" id="from_date1" placeholder="DD.MM.YYYY"
-                       value="{{ \Carbon\Carbon::now()->startOfYear()->format('d.m.Y') }}">
+                <input type="text" class="form-control datepicker1" id="from_date1" placeholder="DD-MM-YYYY"
+                       value="">
             </div>
             <div class="col">
                 <input type="text" class="form-control datepicker1" id="to_date1"
-                       placeholder="DD.MM.YYYY" {{ \Carbon\Carbon::now()->endOfYear()->format('d.m.Y') }}>
+                       placeholder="DD-MM-YYYY" value="" >
             </div>
             {{--            <div class="col">--}}
             {{--                <input type="text" placeholder="Search" class="form-control">--}}
@@ -885,32 +759,8 @@ label{
                     <div class="card-body">
                         <div class="d-flex justify-content-between align-items-baseline mb-2">
                             <h6 class="card-title mb-0" style="text-align: justify;     text-transform: math-auto !important;  color: blue;font-size: 13px; ">Sales Analytics</h6>
-                            {{--                            <div class="dropdown mb-2">--}}
-                            {{--                                <button class="btn p-0" type="button" id="dropdown  MenuButton4"--}}
-                            {{--                                        data-bs-toggle="dropdown"--}}
-                            {{--                                        aria-haspopup="true" aria-expanded="false">--}}
-                            {{--                                    <i class="icon-lg text-muted pb-3px" data-feather="more-horizontal"></i>--}}
-                            {{--                                </button>--}}
-                            {{--                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton4">--}}
-                            {{--                                    <a class="dropdown-item d-flex align-items-center"--}}
-                            {{--                                       href="{{ env('BASE_URL') }}influencers"><i--}}
-                            {{--                                            data-feather="eye" class="icon-sm me-2"></i> <span class="">View</span></a>--}}
-                            {{--                                    <a class="dropdown-item d-flex align-items-center"--}}
-                            {{--                                       href="{{ env('BASE_URL') }}influencers"><i--}}
-                            {{--                                            data-feather="edit-2" class="icon-sm me-2"></i> <span--}}
-                            {{--                                            class="">Edit</span></a>--}}
-                            {{--                                    <a class="dropdown-item d-flex align-items-center"--}}
-                            {{--                                       href="{{ env('BASE_URL') }}influencers"><i--}}
-                            {{--                                            data-feather="trash" class="icon-sm me-2"></i> <span class="">Delete</span></a>--}}
-                            {{--                                    <a class="dropdown-item d-flex align-items-center"--}}
-                            {{--                                       href="{{ env('BASE_URL') }}influencers"><i--}}
-                            {{--                                            data-feather="printer" class="icon-sm me-2"></i> <span class="">Print</span></a>--}}
-                            {{--                                    <a class="dropdown-item d-flex align-items-center"--}}
-                            {{--                                       href="{{ env('BASE_URL') }}influencers"><i--}}
-                            {{--                                            data-feather="download" class="icon-sm me-2"></i> <span--}}
-                            {{--                                            class="">Download</span></a>--}}
-                            {{--                                </div>--}}
-                            {{--                            </div>--}}
+                           
+                            
                         </div>
                         {{-- <p class="text-muted">Sales</p> --}}
                         <div id="monthlySalesChart"></div>
